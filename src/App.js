a6f36d26306b4d86fe48e50fdef586acb3d7ca34
hicham maaqoul <hicham.maaqoul@gmail.com>
2023-11-28 16:05:48 +0100

    configure routes and add homepage to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/lesson/:section/:title">
+          {/* Component to display specific lesson content */}
+        </Route>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
